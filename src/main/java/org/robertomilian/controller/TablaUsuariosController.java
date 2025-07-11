69eaf3f0001675ce270c354c397c9c84d029f620
marcss-bnajera <mbeteta-2024227@kinal.edu.gt>
2025-07-10 20:16:21 -0600

    Agregando el .java controller de TablaUsuarios

---
+++ src/main/java/org/robertomilian/controller/TablaUsuariosController.java
@@ -0,0 +1,21 @@
+package org.robertomilian.controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Marcos
+ */
+public class TablaUsuariosController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
